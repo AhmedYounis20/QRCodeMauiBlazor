6ba92747dcc581e21f6cf46e0501d47161c67b80
Ahmed Younis <ayounis@innotech.com.eg>
2022-09-22 17:26:30 +0200

    Add project files.

---
+++ QRCodeMauiBlazor/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QRCodeMauiBlazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ QRCodeMauiBlazor/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using QRCodeMauiBlazor.Data;
+using ZXing.Net.Maui;
+
+namespace QRCodeMauiBlazor;
+
+public static class MauiProgram
+{
+
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseBarcodeReader()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			})
+        #region
+            .ConfigureMauiHandlers(h =>
+            {
+                h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
+                h.AddHandler(typeof(ZXing.Net.Maui.Controls.CameraView), typeof(CameraViewHandler));
+                h.AddHandler(typeof(ZXing.Net.Maui.Controls.BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
+            });
+		#endregion;
+		builder.Services.AddScoped(Span =>new HttpClient());
+        builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
